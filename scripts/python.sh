b4be1980517daeb92abd58724a14bc75d6927b7d
samuelyuma <samuelyuma.117@gmail.com>
2024-12-05 10:12:26 +0700

    fix: python3.11 installation script

--- scripts/python.sh
+++ scripts/python.sh
@@ -5,6 +5,6 @@ echo "Installing python3.11..."
 sudo apt update
 sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:deadsnakes/ppa -y
-sudo apt install python3.11 -y
+sudo apt install python3.11 python3.11-venv python3.11-dev -y
 
 python3.11 --version
